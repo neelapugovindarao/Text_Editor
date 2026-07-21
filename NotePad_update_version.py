a0eed3dda5985087e48c03a05db758c355a16e8b
neelapugovindarao <neelapugovindarao98@gmail.com>
2026-07-21 19:58:34 +0530

    Implement text area with scrollbar in Notepad
    Added a text area with scrollbar to the Notepad application.

--- NotePad_update_version.py
+++ NotePad_update_version.py
@@ -15,6 +15,14 @@ scrollbar = tk.Scrollbar(frame)
 
 scrollbar.pack(side = "right" , fill = "y")
 
+text_area = tk.Text(frame , wrap = "word" , font = ("consolas" , 15) , 
+                    undo = True , maxundo = 50 , yscrollcommand = scrollbar.set)
+text_area.pack(expand = True , fill = "both")
+
+scrollbar.configure(command = text_area.yview)
+
+
+
 
 root.mainloop()
 
